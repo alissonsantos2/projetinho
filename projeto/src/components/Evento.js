1c0239631b7397c1cb9f7dad76bf6e8c407df6c5
Alisson Santana <alisson.santos2@hotmail.com>
2022-05-26 12:26:47 -0300

    subindo

--- projeto/src/components/Evento.js
+++ projeto/src/components/Evento.js
@@ -1,5 +1,14 @@
-function Evento(){
+function Evento() {
+    function meuEvento() {
+        console.log('Opa, fui ativado!')
+    }
 
+    return (
+        <div>
+            <p>Clique para disparar em um evento:</p>
+            <button onclick={meuEvento} >Ativar!</button>
+        </div>
+    )
 }
 
 export default Evento
